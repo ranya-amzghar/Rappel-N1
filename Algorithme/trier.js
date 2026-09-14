3187084c0a62bc0c4d8490f228d8bb5ce64f98b3
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-09-14 15:06:49 +0100

    Algorithmes

--- Algorithme/trier.js
+++ Algorithme/trier.js
@@ -2,9 +2,11 @@ let arr = [8, 3, 6, 1, 5, 7];
 console.log("Avant:",arr);
 for(i=0;i<arr.length;i++){
     for(j=0;j<arr.length;j++){
-        if(arr[i]<arr[j]){
-            
-        }
+        if(arr[j]>arr[i]){
+            let temp=arr[i];
+             arr[i]=arr[j];
+             arr[j]=temp;
+         }
     }
 }
-console.log("after", arr);
+console.log("After:", arr);
