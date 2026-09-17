4afe92ed842c8d3bbe1a5f8cfdbe9f3ae305585c
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-09-17 10:52:13 +0100

    Algorithme

--- Algorithme/problem.js
+++ Algorithme/problem.js
@@ -6,13 +6,59 @@ let produits = [
     {"produit":"E", "prix":10, "views": 900},
     {"produit":"F", "prix":25, "views": 2000}
 ]
-let badget = 100;
-produits.forEach(produit=>{
-    produit.forEach(views => {
-        if(produit[views]>views){
-            let temp = views;
-            views = produit[views];
-            produit[views] = temp;
+let populaires = [];
+let taille = 0;
+
+for (let i = 0; i < produits.length; i++) {
+
+    if (produits[i].views > 1500) {
+
+        populaires[taille] = produits[i];
+
+        taille++;
+    }
+}
+console.log("Populaire produits: ",populaires);
+
+for (let i = 0; i < populaires.length - 1; i++) {
+
+    for (let j = i + 1; j < populaires.length; j++) {
+
+        if (populaires[i].prix > populaires[j].prix) {
+
+            let temp = populaires[i];
+
+            populaires[i] = populaires[j];
+
+            populaires[j] = temp;
         }
-    });
-})
+    }
+}
+console.log("Trier les produits: ",populaires);
+let budget = 100;
+let total = 0;
+
+let produitsAchetes = [];
+let nombreAchetes = 0;
+
+for (let i = 0; i < populaires.length; i++) {
+
+    if (total + populaires[i].prix <= budget) {
+
+        produitsAchetes[nombreAchetes] = populaires[i];
+
+        nombreAchetes++;
+
+        total = total + populaires[i].prix;
+    }
+}
+
+console.log("Produits achetés :");
+
+for (let i = 0; i < produitsAchetes.length; i++) {
+
+    console.log(produitsAchetes[i].produit);
+}
+
+console.log("Nombre :", nombreAchetes);
+console.log("Total :", total, "DH");
--- prototype/projet_technique/ajouter.php
+++ prototype/projet_technique/ajouter.php
@@ -1,31 +1,31 @@
 <?php
 require 'config.php';
-$rqt= "SELECT * FROM section";
-$st = $pdo->query($rqt);
+$sql1 = 'SELECT * FROM section';
+$st = $pdo->query($sql1);
 $sections = $st->fetchAll(PDO::FETCH_ASSOC);
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['action']) && $_POST['action'] === 'ajouter') {
-        $title = $_POST['title'];
+if($_SERVER['REQUEST_METHOD']=== 'POST'){
+    if(isset($_POST['action']) && $_POST['action'] ==='ajouter'){
+        $titre= $_POST['titre'];
         $contenu = $_POST['contenu'];
-        $duree = $_POST['duree'];
-        $img = $_POST['img'];
-        $status = $_POST['status'];
         $section = $_POST['section'];
-        $sql = "INSERT INTO article(titre_article,contenu_article,duree_lecture,image_article,status,id_section)VALUES(:titre_article,:contenu_article,:duree_lecture,:image_article,:status,:id_section)";
-        $stmt= $pdo->prepare($sql);
-        $stmt -> execute([
-            'titre_article' => $title,
-            'contenu_article' => $contenu,
-            'duree_lecture' => $duree,
-            'image_article' => $img,
-            'status' => $status,
-            'id_section' => $section
-        ]);
-        header('Location: index.php');
-        exit;
+        $status = $_POST['status'];
+        if(!empty($titre) && !empty($contenu) && !empty($section)){
+            $sql2= 'INSERT INTO article(titre_article,contenu_article,id_section,status) VALUES (:titre_article,:contenu_article,:id_section,:status)';
+            $stmt = $pdo->prepare($sql2);
+            $stmt->execute([
+                'titre_article' => $titre,
+                'contenu_article' => $contenu,
+                'id_section' => $section,
+                'status' => $status
+            ]);
+            header('Location: index.php');
+            exit;
+        }else{
+            echo 'Remplissez tout les champs';
+        }
     }else{
         header('Location: index.php');
-        exit;
+            exit;
     }
 }
 ?>
@@ -38,29 +38,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <form method="post">
-        <label for="title">Titre de l'article</label>
-        <input type="text" name="title" id="title">
+        <label for="titre">Titre</label>
+        <input type="text" name="titre" id="titre">
         <label for="contenu">Contenu</label>
-        <input type="text" name="contenu" id="contenu">
+        <textarea name="contenu" id="" cols="30" rows="10"></textarea>
+        <label for="section">Section</label>
+        <select name="section" id="section">
+            <?php
+                foreach($sections as $section){
+                    echo "<option value='".$section['id_section']."'>".$section['section_nom']."</option>";
+                }
+            ?>
+        </select>
         <label for="status">Status</label>
         <select name="status" id="status">
             <option value="brouillon">brouillon</option>
             <option value="publier">publier</option>
         </select>
-        <label for="section">Section</label>
-        <select name="section" id="section">
-        <?php
-            foreach($sections as $section){
-                echo "<option value='".$section['id_section']."'>".$section['section_nom']."</option>";
-            }
-        ?>
-        </select>
-        <label for="duree">Duree de lecture</label>
-        <input type="number" name="duree" id="">
-        <label for="img">Image</label>
-        <input type="file" name="img" id="">
-        <button name='action' value='annuler'>Annuler</button>
         <button name='action' value='ajouter'>Ajouter l'article</button>
+        <button name='action' value='annuler'>Annuler</button>
     </form>
 </body>
 </html>
