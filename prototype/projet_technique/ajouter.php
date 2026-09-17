89f30b4d64d2b26be820f6ddd58578bb692d9402
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-09-17 10:52:39 +0100

    prototype

--- prototype/projet_technique/ajouter.php
+++ prototype/projet_technique/ajouter.php
@@ -35,6 +35,7 @@ if($_SERVER['REQUEST_METHOD']=== 'POST'){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="ajouter.css">
 </head>
 <body>
     <form method="post">
